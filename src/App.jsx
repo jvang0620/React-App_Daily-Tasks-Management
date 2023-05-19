f13b187795c22ac96311c39496989096e83afd7c
Jai_Vang <jaivang0387@gmail.com>
2023-05-19 13:50:48 -0400

    adding delect functionality

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,17 @@ export default function App() {
     })
   }
 
+  //deleteTodo function
+  function deleteTodo(id) {
+    //use all current todos 
+    setTodos(currentTodos => {
+      //take currentTodos and filter them
+      //return all currentTodos except the one being removed
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+
+  }
+
   return (
     <> 
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -64,7 +75,12 @@ export default function App() {
               {todo.title}
 
             </label>
-            <button className='btn btn-danger'>Delete</button>
+            <button 
+            onClick={() => deleteTodo(todo.id)} 
+            className='btn btn-danger'
+            >
+              Delete
+            </button>
           </li>
           )
         })}
